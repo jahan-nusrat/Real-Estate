efe4d7aabbbc8d02d80fb796bf4838a8ad96e883
jahan-nusrat <njnusrat53@gmail.com>
2022-01-03 02:16:09 +0100

    get server side data and display

--- pages/search.js
+++ pages/search.js
@@ -6,10 +6,12 @@ import { BsFilter } from 'react-icons/bs'
 import SearchFilters from '../components/SearchFilters';
 import Property from '../components/Property';
 import noResult from '../assets/images/no-result.svg';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 const Search = ({ properties }) => {
   const [ searchFilters, setSearchFilters ] = useState(false);
   const router =  useRouter();
+  console.log('router', router);
 
   return (
     <Box>
@@ -34,7 +36,7 @@ const Search = ({ properties }) => {
       <Text fontSize={"2xl"} p={"4"} fontWeight={"bold"}>Properties {router.query.purpose}</Text>
       
       <Flex flexWrap={"wrap"}>
-        {properties.map((property) => <Property property={property} key={property.id} />)}
+        {properties.map((property,id) => <Property property={property} key={id} />)}
       </Flex>
       
       {properties.length === 0 && (
@@ -47,14 +49,23 @@ const Search = ({ properties }) => {
   );
 };
 
-export async function getStaticProps() {
-  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
-  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+export async function getServerSideProps({ query }) {
+  const purpose = query.purpose || 'for-rent';
+  const rentFrequency = query.rentFrequency || 'yearly';
+  const minPrice = query.minPrice || '0';
+  const maxPrice = query.maxPrice || '1000000';
+  const roomsMin = query.roomsMin || '0';
+  const bathsMin = query.bathsMin || '0';
+  const sort = query.sort || 'price-desc';
+  const areaMax = query.areaMax || '35000';
+  const locationExternalIDs = query.locationExternalIDs || '5002';
+  const categoryExternalID = query.categoryExternalID || '4';
+
+  const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
 
   return {
     props: {
-      propertiesForSale: propertyForSale?.hits,
-      propertiesForRent: propertyForRent?.hits,
+      properties: data?.hits,
     },
   };
 }
