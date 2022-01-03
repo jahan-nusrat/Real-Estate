7a1cac8122eb5ca9d041319becc3427dc7a0219c
jahan-nusrat <njnusrat53@gmail.com>
2022-01-03 02:51:00 +0100

    filtered options and items component done

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,14 +1,35 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { Box, Text, Flex, Icon, Button, Select, Input, Spinner } from '@chakra-ui/react';
+import { Box, Text, Flex, Icon, Button, Select, Input, Spinner, filter } from '@chakra-ui/react';
 import { MdCancel } from 'react-icons/md';
 import Image from "next/image";
+import { filterData } from '../utils/filterData';
 
 const SearchFilters = () => {
-  const [filters, setFilters] = useState({})
+  const [filters, setFilters] = useState({});
+
+  const searchProperties = (filterValues) => {
+
+  };
+
   return (
     <Flex bg={"gray.100"} p={"4"} justifyContent={"center"} flexWrap={"wrap"}>
-      Search filters
+      {filterData.map((data) => (
+        <Box key={data.queryName}>
+          <Select 
+            placeholder={data.placeholder}
+            w={"fit-content"}
+            p={"2"}
+            onChange={e => searchProperties({[data.queryName] : e.target.value})}
+          >
+            {data.items.map((item) => (
+              <option key={item.value} value={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
     </Flex>
   )
 };
