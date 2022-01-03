36d442d31a534dd3f207d7774e5ad0d3caaad2de
jahan-nusrat <njnusrat53@gmail.com>
2022-01-03 02:40:01 +0100

    dummy data created

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -5,10 +5,11 @@ import { MdCancel } from 'react-icons/md';
 import Image from "next/image";
 
 const SearchFilters = () => {
+  const [filters, setFilters] = useState({})
   return (
-    <div>
+    <Flex bg={"gray.100"} p={"4"} justifyContent={"center"} flexWrap={"wrap"}>
       Search filters
-    </div>
+    </Flex>
   )
 };
 
